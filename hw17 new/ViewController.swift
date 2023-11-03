ba6255fd08c3448d7b767afb8d9cc99d8c3245d5
Dilnura Rizayeva <didi.riri2004@mail.ru>
2023-11-03 19:15:22 +0600

    label create

--- hw17 new/ViewController.swift
+++ hw17 new/ViewController.swift
@@ -1,9 +1,35 @@
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    //MARK:  Label
+
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Number"
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpViews()
+    }
+
+    //MARK: SetUp Views
+        private func setUpViews() {
+            view.addSubview(label)
+        }
+
+    //MARK: SetUp Constraints
+    private func setUpConstraints() {
+        label.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(200)
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+        }
     }
 }
 
