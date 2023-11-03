deab9718eb9edc49f29cc682be65360bd2b68e01
Dilnura Rizayeva <didi.riri2004@mail.ru>
2023-11-03 18:23:29 +0600

    Initial Commit

---
+++ hw17 new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw17 new
+//
+//  Created by Dilnura Rizaeva on 03.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
