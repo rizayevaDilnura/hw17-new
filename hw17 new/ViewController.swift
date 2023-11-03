7132f40486e9f94c5f4c83d16c6c8e81cadf9131
Dilnura Rizayeva <didi.riri2004@mail.ru>
2023-11-03 18:46:00 +0600

    deleted storyboard and comments

--- hw17 new/ViewController.swift
+++ hw17 new/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  hw17 new
-//
-//  Created by Dilnura Rizaeva on 03.11.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
